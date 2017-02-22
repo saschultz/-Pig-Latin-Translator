cb1c1f9779e4e59bcde71c63dc74a3e58848272d
Evan Bishop and Sara Schultz <evan@>
2017-02-22 11:38:47 -0800

    pass spec to identify vowels and consonants in first index of array and push that consonant to the end of word and then push ay

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,33 @@
 
 $(document).ready(function(){
   $("#formOne").submit(function(){
-
-    var userInput = $("#userinput").val();
-    // var transform = function(text){
-
-
+      var userInput = $("#userinput").val();
       var splitString = userInput.split("");
-      var firstLetter = splitString[0]
-      if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u"){
+      var firstLetterVowel = splitString[0]
+      var firstLetterConsonant = splitString[0]
+      if (firstLetterVowel === "a" || firstLetterVowel === "e" || firstLetterVowel === "i" || firstLetterVowel === "o" || firstLetterVowel === "u"){
         splitString.push("ay");
       }
+      else if (firstLetterConsonant === "b" || firstLetterConsonant === "c" || firstLetterConsonant === "d" || firstLetterConsonant === "f" || firstLetterConsonant === "g" || firstLetterConsonant === "h" || firstLetterConsonant === "j" || firstLetterConsonant === "k" || firstLetterConsonant === "l"){
+        splitString.push(splitString[0]);
+        splitString.splice(0, 1)
+        splitString.push("ay");
+        console.log(splitString);
+      } else if (firstLetterConsonant === "m" || firstLetterConsonant === "n" || firstLetterConsonant === "p" || firstLetterConsonant === "q" || firstLetterConsonant === "r" || firstLetterConsonant === "s" || firstLetterConsonant === "t" || firstLetterConsonant === "v" || firstLetterConsonant === "w" || firstLetterConsonant === "x" || firstLetterConsonant === "y"){
+        splitString.push(splitString[0]);
+        splitString.splice(0, 1)
+        splitString.push("ay");
+        console.log(splitString);
+      } else if (firstLetterConsonant === "z"){
+        splitString.push(splitString[0]);
+        splitString.splice(0, 1)
+        splitString.push("ay");
+        console.log(splitString);
+      }else{
 
+      }
       var join = splitString.join("");
       console.log(join);
-    // }
-    // transform(userInput);
-
-
     $("#result").text(join);
     $("#result").show();
 
