32bca4968cc06665bcae5968cc1d8b7598fd90d7
Evan Bishop and Sara Schultz <sara@>
2017-02-22 10:39:43 -0800

    pass next spec of adding ay to a word beginning with a lower case vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,31 @@
+
+
+
+
 $(document).ready(function(){
   $("#formOne").submit(function(){
 
-    var userInput = $("#pigLatin").val();
-    var pigLatin = function(text){
-      $("#result").text(userInput);
-    }
-    pigLatin(userInput);
+    var userInput = $("#userinput").val();
+    // var transform = function(text){
 
 
+      var splitString = userInput.split("");
+      var firstLetter = splitString[0]
+      if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u"){
+        splitString.push("ay");
+      }
+
+      var join = splitString.join("");
+      console.log(join);
+    // }
+    // transform(userInput);
 
 
-
-
-
-
-
+    $("#result").text(join);
     $("#result").show();
 
     event.preventDefault();
   })
 })
+
+// "ay".push
